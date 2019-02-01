eccd0faacd0408505833db261950e052149e85f6
Adam Pope <adam1.pope@gmail.com>
2019-01-31 20:08:35 -0600

    ...

--- lib/pope_view_tool/renderer.rb
+++ lib/pope_view_tool/renderer.rb
@@ -1,9 +1,9 @@
-module PopeViewTool
-  class Renderer
-    def self.copyright name, message
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{message}".html_safe
-    end 
-  end 
-end  
+# module PopeViewTool
+#   class Renderer
+#     def self.copyright name, message
+#       "&copy; #{Time.now.year} | <b>#{name}</b> #{message}".html_safe
+#     end 
+#   end 
+# end  
  
  
